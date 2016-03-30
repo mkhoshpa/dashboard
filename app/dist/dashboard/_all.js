64790d295957ee9a1d921cfc013af27033faa427
Thom <me@thomlamb.com>
2016-03-30 18:19:10 -0300

    thom's first commit

--- app/dist/dashboard/_all.js
+++ app/dist/dashboard/_all.js
@@ -1,1 +1,12 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="boot.ts" />
+/// <reference path="models.ts" />
+/// <reference path="controllers/mainController.ts" />
+/// <reference path="controllers/addUserDialogController.ts" />
+/// <reference path="controllers/reminderController.ts" />
+/// <reference path="controllers/slackUsersController.ts" />
+/// <reference path="controllers/contactPanelController.ts" />
+// Reference Users Namespace /  app.users module
+/// <reference path="../users/services/userService.ts"/>
+/// <reference path="../users/_all.ts"/>
 //# sourceMappingURL=_all.js.map

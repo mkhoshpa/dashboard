8b1f808b350e0ee5b8d74148d6ee6f09e4ed0b4d
Thom <me@thomlamb.com>
2016-03-31 17:30:58 -0300

    added endpoint for reminders, and changed reminderschema

--- server/routes/api/reminders.js
+++ server/routes/api/reminders.js
@@ -8,4 +8,7 @@ var reminder = require('../../controllers/api/reminder.js');
 module.exports = function(app) {
   app.post('/willow/reminder', reminder.create);
   app.get('/willow/reminder', reminder.list);
+  app.get('/willow/reminder/now', reminder.listNow);
+
+//  app.get('/willow/reminder/now', reminder.now);
 }

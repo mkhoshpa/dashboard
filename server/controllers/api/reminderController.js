d9f43d4f5e698bd4998961886a1cb06971a02ee1
Shane Pelletier <lotios611@gmail.com>
2016-05-26 20:23:14 -0300

    Stuff

--- server/controllers/api/reminderController.js
+++ server/controllers/api/reminderController.js
@@ -177,7 +177,7 @@ exports.update = function(req, res) {
   console.log();
   console.log(JSON.stringify(req.body));
   User.findOneAndUpdate(
-    {"_id": req.body.assignee,/* "reminders.$._id": req.body._id*/},
+    {"_id": req.body.assignee, "reminders.$._id": req.body._id},
     {
       "$set": {
         "reminders.$": req.body

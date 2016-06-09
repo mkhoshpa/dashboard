61634c6fe47c82d64d99cc1b9e5a447dd169a4d3
Chops <fitnessevangelist@gmail.com>
2016-06-09 09:47:51 -0300

    fixing typo in development.js

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -3,7 +3,7 @@
 
 // Set the 'development' environment configuration object
 module.exports = {
-<<<<<<< HEAD
+
   // If I push this code to dev and the db is still localhost I owe Josh + Thom $5 each - Shane
 	db: 'mongodb://thom:letmein1@ds011251.mlab.com:11251/fitpath',
 	sessionSecret: 'developmentSessionSecret',

a0e47739a29ffad6ec871cf77f414084304bdc34
Shane Pelletier <lotios611@gmail.com>
2016-06-09 16:11:12 -0300

    Working on fixing DO

--- server/config/strategies/local.js
+++ server/config/strategies/local.js
@@ -17,6 +17,14 @@ module.exports = function() {
         });
       }
       if (!user.authenticate(password)) {
+        console.log();
+        console.log('Something is wrong with the user. The user is:');
+        console.log(JSON.stringify(user));
+        console.log('The user\'s password is:');
+        console.log(user.password);
+        console.log('And the password that was attempted was: ');
+        console.log(user.hashPassword(password));
+        console.log();
         return done(null, false, {
           message: 'Invalid password'
         });

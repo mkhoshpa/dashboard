de2a0796c98bc598e084e2eee49e7f4ff98d7bd9
Shane M. Pelletier <lotios611@gmail.com>
2016-06-09 12:37:27 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('nodemon' ,['sass'], function (cb) {
 gulp.task('browser-sync', ['nodemon'], function() {
 	browserSync.init(null, {
     injectChanges: true,
-		proxy: "http://localhost:8081",
+		proxy: "http://localhost:9812",
         files: ["app/**/*.*"],
         browser: 'google chrome',
         port: 7000,

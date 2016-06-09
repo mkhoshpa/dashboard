de105e0f47d46bdbe68b0d8a5d452d039a896c6c
Shane M. Pelletier <lotios611@gmail.com>
2016-06-09 12:38:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ var passport = passport();
 
 
 
-// Use the Express application instance to listen to the '8081' port
-app.listen(8081);
+// Use the Express application instance to listen to the '9812' port
+app.listen(9812);
 
 // Log the server status to the console
-console.log('Server running at http://localhost:8081/');
+console.log('Server running at http://localhost:9812/');
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;

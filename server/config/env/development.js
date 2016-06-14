11203a47ef7bc269aefe0c7d0e59e8c5df301f12
Shane Pelletier <lotios611@gmail.com>
2016-06-14 13:01:12 -0300

    I messed up

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -4,10 +4,10 @@
 // Set the 'development' environment configuration object
 //'mongodb://thom:letmein1@ds011251.mlab.com:11251/fitpath'
 module.exports = {
-  // If I push this code to dev and the db is still localhost I owe Josh + Thom $5 each - Shane
+  // If I push this code to dev and the db is still 107.170.21.178 I owe Josh + Thom $5 each - Shane
 	//db: 'mongodb://thom:letmein1@ds011251.mlab.com:11251/fitpath',
 	//db: 'mongodb://shane:letmein1@ds013024.mlab.com:13024/prod',
-  db: 'mongodb://shane:letmein1@localhost:27017/development',
+  db: 'mongodb://shane:letmein1@107.170.21.178:27017/development',
   sessionSecret: 'developmentSessionSecret',
   phoneNumbers: {
     messages: '+12898062194',
@@ -16,16 +16,16 @@ module.exports = {
 	facebook: {
 		clientID: '237058900007908',
 		clientSecret: '756eb97b58fb72f845277f0e2f51fda2',
-		callbackURL: 'http://localhost:12557/oauth/facebook/callback'
+		callbackURL: 'http://107.170.21.178:12557/oauth/facebook/callback'
 	},
 	twitter: {
 		clientID: 'Twitter Application ID',
 		clientSecret: 'Twitter Application Secret',
-		callbackURL: 'http://localhost:8081/oauth/twitter/callback'
+		callbackURL: 'http://107.170.21.178:8081/oauth/twitter/callback'
 	},
 	google: {
 		clientID: 'Google Application ID',
 		clientSecret: 'Google Application Secret',
-		callbackURL: 'http://localhost:8081/oauth/google/callback'
+		callbackURL: 'http://107.170.21.178:8081/oauth/google/callback'
 	}
 };

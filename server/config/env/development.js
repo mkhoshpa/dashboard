f92d0fb1f5c7e78b58c62600bc5dfa974019f900
Shane M. Pelletier <lotios611@gmail.com>
2016-06-14 13:08:01 -0300

    I owe Thom + Josh $5 each

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -4,10 +4,10 @@
 // Set the 'development' environment configuration object
 //'mongodb://thom:letmein1@ds011251.mlab.com:11251/fitpath'
 module.exports = {
-  // If I push this code to dev and the db is still 107.170.21.178 I owe Josh + Thom $5 each - Shane
+  // If I push this code to dev and the db is still localhost I owe Josh + Thom $5 each - Shane
 	//db: 'mongodb://thom:letmein1@ds011251.mlab.com:11251/fitpath',
-	//db: 'mongodb://shane:letmein1@ds013024.mlab.com:13024/prod',
-  db: 'mongodb://shane:letmein1@107.170.21.178:27017/development',
+	db: 'mongodb://shane:letmein1@ds013024.mlab.com:13024/prod',
+  //db: 'mongodb://shane:letmein1@localhost/development',
   sessionSecret: 'developmentSessionSecret',
   phoneNumbers: {
     messages: '+12898062194',

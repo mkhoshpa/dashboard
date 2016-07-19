c4c3dac46238794f38e879b932bd7be0162800e8
Shane Pelletier <lotios611@gmail.com>
2016-07-19 10:08:23 -0300

    Added RabbitMQ post('save') hook to response.js

--- server/models/response.js
+++ server/models/response.js
@@ -2,6 +2,7 @@
 //not used
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var amqp = require('amqplib/callback_api');
 
 //response schema is super dumb and doesn't figure out status by itself -
 //might not be a bad idea to make a status model actually
@@ -40,6 +41,18 @@ var responseSchema = new Schema({
   ],
   reminderId: {type: mongoose.Schema.Types.Object, ref: 'Reminder'}
 
+});
+
+responseSchema.post('save', function (response) {
+  amqp.connect('amqp://localhost', function (err, conn) {
+    conn.createChannel(function (err, channel) {
+      var queue = 'responses';
+
+      ch.assertQueue(queue, {durable: true});
+      // new Buffer(JSON.stringify(response.toObject())) converts the document from mongodb to a buffer
+      ch.sendToQueue(queue, new Buffer(JSON.stringify(response.toObject())), {persistent: true});
+    });
+  });
 });
 
 var Response = mongoose.model('Response', responseSchema);

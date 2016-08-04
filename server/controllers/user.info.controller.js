f38cee3ece187be5132bb417ced29702248e834c
Jlaver <laverjoshua@gmail.com>
2016-08-04 16:16:43 -0300

    still not working with null

--- server/controllers/user.info.controller.js
+++ server/controllers/user.info.controller.js
@@ -101,10 +101,20 @@ exports.createPhoneNumber = function (req, res) {
   {safe: true},
   function (err, user) {
     if (err) {
-      winston.error(err);
+      //console.log(err);
+      if (err.name === 'MongoError' && err.code === 11000) {
+        // Duplicate username
+        console.log("Am i in here");
+        res.status(500).send({ success: false, message: 'User already exist!' });
+      }
+      res.status(500).send(err);
+    }
+    else{
+      console.log("good");
+      res.send(req.body.number);
     }
   });
-  res.send(req.body.number);
+
 };
 
 exports.addSurvey = function(req, res){
@@ -120,7 +130,68 @@ exports.addSurvey = function(req, res){
   res.send(req.body);
 };
 
-exports.create = function(req, res) {
+exports.create = function (req, res) {
+  console.log("new user");
+  var user = new User(req.body);
+  user.provider = 'local';
+  user.fullName = user.firstName + ' ' + user.lastName;
+  user.pandoraBotSaid = '';
+
+  user.save(function(err, user){
+    if(err){
+      if (err.name === 'MongoError' && err.code === 11000) {
+        // Duplicate username
+        console.log("Am i in here");
+        console.log(err);
+        res.status(500).send({ success: false, message: "Phone number already in use." });
+      }
+      else{
+        console.log(err);
+      }
+    }
+    else{
+      console.log("Good");
+      res.send(user);
+    }
+  })
+
+
+}
+exports.updateCoach = function(req, res){
+  console.log("Im a coach!");
+  console.log(req.params.id);
+  var user = new User(req.body);
+  console.log('ima a saf');
+
+  User.findByIdAndUpdate({_id: req.params.id},
+    {$push: {"clients": user._id}},
+    {safe: true},
+    function(err, coach) {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        console.log('adding user ' + user._id + ' ot coac');
+        user.clients.push(user._id);
+        console.log('success');
+        res.send(user);
+      }
+    });
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+exports.create2 = function(req, res) {
   console.log("Im before new User.");
   //console.log(req.body);
   var user = new User(req.body);
@@ -129,8 +200,14 @@ exports.create = function(req, res) {
   user.pandoraBotSaid = '';
   user.save(function(err) {
     if (err) {
-      winston.error(err);
-      res.send(err);
+      if (err.name === 'MongoError' && err.code === 11000) {
+        // Duplicate username
+        console.log("Am i in here");
+        res.status(500).send({ success: false, message: 'User already exist!' });
+      }
+      else{
+        console.log(err);
+      }
     }
     else {
       console.log(user._id);
@@ -156,12 +233,7 @@ exports.create = function(req, res) {
     });
   };
 
-exports.updateCoach = function(req, res){
-  console.log("Im a coach!");
-  var user = new User(req.body);
-  console.log('ima a saf');
-  res.send(user);
-};
+
 
 
 exports.render = function(req, res) {

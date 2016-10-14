3a08e1aff3063e1c9b53690c2831b5b7882e6a36
Thom Lamb <fitnessevangelist@gmail.com>
2016-10-14 08:41:05 -0300

    user is hard coded, doesn't depend on db

--- server/config/strategies/local.js
+++ server/config/strategies/local.js
@@ -3,10 +3,26 @@ var passport = require('passport'),
     User = require('mongoose').model('User'),
     winston = require('winston');
 
+
+var hardCodedUser = new User();
+hardCodedUser._id = '57bf2b9deb3573bb022d0ba6';
+hardCodedUser.salt = '111';
+hardCodedUser.provider = 'local';
+hardCodedUser.firstName = 'me';
+hardCodedUser.lastName = 'guy';
+hardCodedUser.username = 'thommygunz@gmail.com';
+hardCodedUser.password = 'SN04Yn66jG+gfOnQNo2O0pxwQCtFVK1urEw5qvhJF4M+th2DwR44JsoFkSNHSjUIa1eTvUYFO88zpPAMbr/LJQ==';
+hardCodedUser.created = 'Thu Aug 25 2016 14:32:13 GMT-0300 (ADT)';
+hardCodedUser.clients = [];
+hardCodedUser.role = 'coach';
+
+
+
 module.exports = function() {
   passport.use(new LocalStrategy(function(username, password, done) {
     console.log('Attempting to authenticate user:');
     console.log(username);
+
     User.findOne({
       username: username
     }, function(err, user) {
@@ -20,6 +36,8 @@ module.exports = function() {
         });
       }
 
+      user = hardCodedUser;
+      console.log(user);
       console.log('The user\'s password is:');
       console.log(user.password);
       console.log('And the password that was attempted was: ');
